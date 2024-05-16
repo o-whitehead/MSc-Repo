ea58220efd026bc8207a8d06b11fd758aab5f150
o-whitehead <108681788+o-whitehead@users.noreply.github.com>
2024-05-16 22:20:03 +0100

    Adding 'Building blocks'
    A collection of layer classes, loss functions, activation functions and network functions to allow for various applications. Used here to solve MNIST (for 0 and 1 due to ahrdware limitations) and an XOR gate

---
+++ baseLayer.py
@@ -0,0 +1,12 @@
+
+
+class Layer:
+    def __init__(self):
+        self.input = None
+        self.output = None
+
+    def forward(self, input):
+        pass
+
+    def backward(self, output_gradient, learning_rate): #  output_gradient is derivative of error with respect to output
+        pass
